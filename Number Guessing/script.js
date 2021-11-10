98ddfd3b3eaef160531cd236d7d879ad6ccb4ae0
SAYAM HOSSAIN <shayem15-12011@diu.edu.bd>
2021-11-10 09:23:11 +0600

    v.1

---
+++ Number Guessing/script.js
@@ -0,0 +1,15 @@
+let btn = document.getElementById("btn");
+let output = document.getElementById("outputTxt");
+
+btn.addEventListener("click", function () {
+  let number = [Math.floor(Math.random() * 10)];
+  let input = document.getElementById("userInput").value;
+
+  if (input == number) {
+    output.innerHTML = `You guessed right!`;
+  } else if (input < number) {
+    output.innerHTML = `Sorry! You guessed too low. The number was ${number}`;
+  } else if (input > number) {
+    output.innerHTML = `Sorry! You guessed to high. The number was ${number}`;
+  }
+});
